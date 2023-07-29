549c5e9c46e4707e2ba9bedf64332821a92c0e23
Shubhangi Goswami <108186053+shubhangi0001@users.noreply.github.com>
2023-07-29 21:15:55 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import BackgroundColorToggler from './BackgroundColorToggler';
+
+function App() {
+  return (
+    <div className="App">
+      <BackgroundColorToggler />
+    </div>
+  );
+}
+
+export default App;
